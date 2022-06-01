d5b4d26fa14e6c51ae05dda5b7524ca9063787d9
henry <aengrhenry@gmail.com>
2022-05-31 23:11:16 -0700

    setting up django allauth, static files, templates and email configuation

--- user_authentication/settings.py
+++ user_authentication/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
+    #local apps
+    'user_profile',
+    'crispy_forms',
 
 
 ]
@@ -80,6 +83,7 @@ AUTHENTICATION_BACKENDS = [
 
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -90,6 +94,27 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#POSTGRESQL SETTINGS
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': ‘<db_name>’,
+
+#         'USER': '<db_username>',
+
+#         'PASSWORD': '<password>',
+
+#         'HOST': '<db_hostname_or_ip>',
+
+#         'PORT': '<db_port>',
+
+#     }
+
+# }
 
 
 # Password validation
@@ -127,6 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "static_cdn"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -138,3 +167,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ACCOUNT_AUTHENTICATION_METHOD="email"
 ACCOUNT_EMAIL_REQUIRED=True
 ACCOUNT_EMAIL_VERIFICATION="optional"
+
+
+
+#Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST=''
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=''
+EMAIL_PORT=''
